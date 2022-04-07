8a6764394ef8f705cb9a1782f754e1eb97c6059d
mauricioprb <mauriciopereirabraga@gmail.com>
2022-04-07 15:00:20 -0300

    msg

--- cursoRoboticaIniciante/acenderLedDelay/src/main.cpp
+++ cursoRoboticaIniciante/acenderLedDelay/src/main.cpp
@@ -1,19 +1,24 @@
+/**
+ * @file main.cpp
+ * @author Mauricio Pereira Braga (mauprb@gmail.com)
+ * @brief Piscar led com intervalo de 1 segundo - Bolsa PROBEX
+ * @version 0.1
+ * @date 2022-04-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <Arduino.h>
 
-void setup() {
+void setup(){
   pinMode(12, OUTPUT);
 }
 
-void loop() {
+void loop(){
   digitalWrite(12, HIGH);
   delay(100);
 
-  digitalWrite(13, HIGH);
-  delay(100);
-
   digitalWrite(12, LOW);
-  delay(100);
-
-  digitalWrite(13, HIGH);
   delay(100);
 }
