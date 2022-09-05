bb4cd8ecc4ab6d2dbe0b4fa1f127f4a1b5917fa2
Mauricio Pereira <mauriciopereirabraga@gmail.com>
2022-09-05 00:54:05 -0300

    comentarios

--- projetos/carro_bluetooth/main.ino
+++ projetos/carro_bluetooth/main.ino
@@ -4,28 +4,31 @@
 
 SoftwareSerial bth(11,12);
 
+// Instanciar objetos das classes
 Motor *frente;
 Motor *re;
 Direcao *direcao;
 Controles *controles;
 
 void setup() {
-    frente = new Motor(2);
+    // Criar objetos com os pinos correspondentes
     re = new Motor(3);
+    frente = new Motor(2);
     direcao = new Direcao(9);
 
+    // Definir as portas dos motores como saída
     pinMode(frente->porta(), OUTPUT);
     pinMode(re->porta(), OUTPUT);
 
-    direcao->porta();
-    direcao->centro();
+    direcao->porta(); // Define porta para o servo
+    direcao->centro(); // Inicia o programa com o servo centralizado
 
-    bth.begin(9600); 
+    bth.begin(9600); // BAUD do bluetooth
 }
 
 void loop() {
-    if (bth.available()) {
-        Serial.write(bth.read());
-        controles->Controles(bth.read());
+    if (bth.available()) { // Se o bluetooth estiver disponível
+        Serial.write(bth.read()); // Defindo leitura do bluetooth com as letras que o app envia
+        controles->Controles(bth.read()); // Acessa a classe com o caratere que o bluetooth envia
     }
 }
